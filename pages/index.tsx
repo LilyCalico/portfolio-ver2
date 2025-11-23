4c4427cf8ad65a07956df57a4052c7da50592fc5
Yuri <106544693+LilyCalico@users.noreply.github.com>
2025-11-24 07:28:21 +0900

    feat: Add Head component for SEO and update page structure in Home component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Experience from "../components/Experience";
 import Footer from "../components/Footer";
 import Projects from "../components/Projects";
@@ -6,17 +7,22 @@ import Top from "../components/Top";
 
 export default function Home() {
   return (
-    <div className="mt-[9.6rem] flex flex-col items-center">
-      <Top />
+    <>
+      <Head>
+        <title>Yuri / Portfolio</title>
+      </Head>
+      <div className="mt-[9.6rem] flex flex-col items-center">
+        <Top />
 
-      <main className="flex flex-col items-start gap-[10rem]">
-        <TechStack />
-        {/* <About /> */}
-        <Experience />
-        <Projects />
-      </main>
+        <main className="flex flex-col items-start gap-[10rem]">
+          <TechStack />
+          {/* <About /> */}
+          <Experience />
+          <Projects />
+        </main>
 
-      <Footer />
-    </div>
+        <Footer />
+      </div>
+    </>
   );
 }
