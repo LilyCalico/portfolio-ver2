dab974436ced8bfc6aea4cb56c35aeab575b0a9e
Yuri <106544693+LilyCalico@users.noreply.github.com>
2025-11-24 20:52:21 +0900

    style: Adjust padding for responsive design

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Header />
-      <div className="px-[8rem]">
+      <div className="px-[4rem] md:px-[8rem]">
         <Component {...pageProps} />
       </div>
     </>
